d6e72a5fa9b4553268915901d81805d4cfa7f173
Michelle Cheng <michellecheng.2017@gmail.com>
2022-04-29 13:45:43 -0400

    edited homepage

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -7,10 +7,15 @@ It constructs a React component to display the home page.
 const HomePageView = () => {
   // Render Home page view
   return (
-    <div >
+    <div>
       <h1>Home Page</h1>
+      <h3>View Campuses and students</h3>
+      <img
+        src="https://thumbs.dreamstime.com/b/university-building-vector-illustration-cartoon-d-outside-front-view-high-elementary-school-college-academy-campus-186210510.jpg"
+        alt="campus"
+      />
     </div>
-  );    
-}
+  );
+};
 
 export default HomePageView;
